54450f8ee5f83926cc7be2a569523fc14863d301
LeamanB <LeamanBrown@gmail.com>
2022-11-14 07:36:53 -0600

    home

---
+++ Week 3/Practice.js
@@ -0,0 +1,10 @@
+function preparePizza()
+const pizza = [
+    { size: "12 inches"}, 
+    { crust: "Whole Wheat"},
+    {...toppings = pizzaToppings}, 
+
+    console.log("Now cooking pizza[pizza.crust]")
+    
+];
+return preparePizza;
